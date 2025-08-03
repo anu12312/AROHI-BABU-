af0cf49da508661f233cabe0d5aeef7d81d509e6
SUSH1L KUM9R Y9D9V 😌🔐❤️ <sushiilkumar103@gmail.com>
2025-08-03 20:43:15 +0500

    Update 0_ARIF-OWNER-MENTION.js

--- models/commands/0_ARIF-OWNER-MENTION.js
+++ models/commands/0_ARIF-OWNER-MENTION.js
@@ -9,7 +9,7 @@ module.exports.config = {
   cooldowns: 1
 };
 module.exports.handleEvent = function({ api, event }) {
-  if (event.senderID !== "61574881089242") {
+  if (event.senderID !== "610265515") {
 ];
     for (const id of aid) {
     if ( Object.keys(event.mentions) == id) {
